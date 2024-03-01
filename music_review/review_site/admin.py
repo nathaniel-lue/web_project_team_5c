bea88606831d37b2fe541481db527ec770a7a433
Steve Falkovsky <2782416F@student.gla.ac.uk>
2024-03-01 11:13:43 +0000

    created models and admin interfaces for everything but user model

--- music_review/review_site/admin.py
+++ music_review/review_site/admin.py
@@ -1,3 +1,51 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+
+class ArtistAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
+
+class AlbumAdmin(admin.ModelAdmin):
+    list_display = ('name', 'artist', 'release_date', 'rating')
+    list_filter = ('artist', 'release_date')
+    search_fields = ('name', 'artist__name')
+
+class EPAdmin(admin.ModelAdmin):
+    list_display = ('name', 'artist', 'release_date', 'rating')
+    list_filter = ('artist', 'release_date')
+    search_fields = ('name', 'artist__name')
+
+class SongAdmin(admin.ModelAdmin):
+    list_display = ('name', 'artist', 'album', 'release_date', 'rating')
+    list_filter = ('artist', 'album', 'release_date')
+    search_fields = ('name', 'artist__name', 'album__name')
+
+class SingleAdmin(admin.ModelAdmin):
+    list_display = ('name', 'artist', 'release_date', 'rating')
+    list_filter = ('artist', 'release_date')
+    search_fields = ('name', 'artist__name')
+
+class GigAdmin(admin.ModelAdmin):
+    list_display = ('artist', 'venue', 'rating')
+    list_filter = ('artist', 'venue')
+    search_fields = ('artist__name', 'venue')
+
+
+# Register the models and their admin classes
+admin.site.register(Artist, ArtistAdmin)
+admin.site.register(Album, AlbumAdmin)
+admin.site.register(EP, EPAdmin)  # If EPs are managed similarly to Albums
+admin.site.register(Song, SongAdmin)
+admin.site.register(Single, SingleAdmin)
+admin.site.register(Gig, GigAdmin)
+
+
+'''
+list_display is used to specify which fields are shown in the list view of each model.
+
+list_filter allows filtering by specified fields in the admin's list view sidebar.
+
+search_fields enables search functionality on specified fields. Note that when searching through 
+foreign key fields, you should use the syntax fieldname__relatedfieldname.
+'''
--- music_review/review_site/models.py
+++ music_review/review_site/models.py
@@ -1,3 +1,47 @@
 from django.db import models
 
-# Create your models here.
+# django automatically creates an ID field for the models
+
+
+class Artist(models.Model):
+    name = models.CharField(max_length=100)
+    
+
+
+class Album(models.Model):
+    artist = models.ForeignKey(Artist, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    release_date = models.DateField()
+    rating = models.DecimalField(max_digits=2, decimal_places=1)  # 5 stars rating
+    
+# since EP is structurally the same as album it just inherits from the album model
+class EP(Album):
+    pass
+    
+
+
+# abstarct class for models of song, single
+class MusicEntity(models.Model):
+    artist = models.ForeignKey(Artist, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    release_date = models.DateField()
+    rating = models.DecimalField(max_digits=2, decimal_places=1)  # 5 stars rating
+
+    class Meta:
+        abstract = True
+        
+# stand alone single
+class Single(MusicEntity):
+    pass
+
+# song which is part of an album
+class Song(MusicEntity):
+    album = models.ForeignKey(Album, on_delete=models.CASCADE)
+
+
+ 
+    
+class Gig(models.Model):
+    artist = models.ForeignKey(Artist, on_delete=models.CASCADE)
+    venue = models.CharField(max_length=100)
+    rating = models.DecimalField(max_digits=2, decimal_places=1)  # 5 stars rating
