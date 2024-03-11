8624b3bd79e4920d17769bd0decebf7a1caace4a
Nathaniel Lue <2778183l@student.gla.ac.uk>
2024-03-11 09:40:58 +0000

    explore page finished

--- music_review/review_site/views.py
+++ music_review/review_site/views.py
@@ -7,3 +7,20 @@ def index(request):
 def explore(request):
     return render(request, 'review_site/explore.html')
 
+def forum(request):
+    return render(request, 'review_site/forum.html')
+
+def artist_profile(request):
+    return render(request, 'review_site/explore/artist_profile.html')
+
+def leaderboard(request):
+    return render(request, 'review_site/leaderboard.html')
+
+def music(request):
+    return render(request, 'review_site/music.html')
+
+def post_review(request):
+    return render(request, 'review_site/postreview.html')
+
+def sign_up_page(request):
+    return render(request, 'review_site/signup.html')
