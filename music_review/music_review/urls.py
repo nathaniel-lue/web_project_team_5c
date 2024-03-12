0a54911bc1ecb71f74e565162a4ca0d0ca13d863
Steve Falkovsky <2782416F@student.gla.ac.uk>
2024-03-12 20:15:49 +0000

    moved templates to same place, commented out some stuff for later

--- music_review/music_review/urls.py
+++ music_review/music_review/urls.py
@@ -7,8 +7,7 @@ urlpatterns = [
 ]
 
   #move all the stuff below to review_site/urls.py
-    path('', views.index, name='index'),
-    path("explore/", views.explore, name='explore'),
-    path("post_review/", views.post_review , name = "post_review")
-]
+    # path('', views.index, name='index'),
+    # path("explore/", views.explore, name='explore'),
+    # path("post_review/", views.post_review , name = "post_review")
 
