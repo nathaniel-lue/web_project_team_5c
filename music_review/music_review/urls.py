ac4919ec50baf7d643058f4091def138d0f82f5d
2766838S <2766838S@student.gla.ac.uk>
2024-03-13 20:07:38 +0000

    changes to urls.py to make website load again

--- music_review/music_review/urls.py
+++ music_review/music_review/urls.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.urls import path
+from review_site import views
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
--- music_review/review_site/urls.py
+++ music_review/review_site/urls.py
@@ -1,12 +1,20 @@
 from django.urls import path, include
 from .views import *
+from django.contrib import admin
+from django.urls import path
+from review_site import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 app_name = 'review_site'
 
 urlpatterns = [
-    path("accounts/", include("django.contrib.auth.urls")), # authentication URLs
-    path('accounts/register/', register, name='register')
-]
+    path('', views.index, name='index'),
+    path("explore/", views.explore, name='explore'),
+    path("post_review/", views.post_review , name = "post_review"),
+    path("accounts/", include("django.contrib.auth.urls")), 
+    path('accounts/register/', register, name='register'),
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) 
 
 
 
