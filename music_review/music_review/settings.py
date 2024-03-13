2e88b6192a2963eae706b1399271a389c916963d
2766838S <2766838S@student.gla.ac.uk>
2024-03-13 20:43:10 +0000

    change to settings

--- music_review/music_review/settings.py
+++ music_review/music_review/settings.py
@@ -91,7 +91,7 @@ WSGI_APPLICATION = "music_review.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "NAME": str(os.path.join(BASE_DIR, "db.sqlit3")),
     }
 }
 
