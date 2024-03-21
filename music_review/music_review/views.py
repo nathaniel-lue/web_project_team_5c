b3312cfd6ff285891209b40aa764e7e37c8e1cf7
Steve Falkovsky <2782416F@student.gla.ac.uk>
2024-03-21 12:31:17 +0000

    test fix + login fix

--- music_review/music_review/views.py
+++ music_review/music_review/views.py
@@ -13,22 +13,3 @@ def register(request):
     else:
         form = UserCreationForm()
     return render(request, 'registration/register.html', {'form': form})
-
-
-
-
-# Not sure if this is needed or not - Will update as I get more stuff done with authentication
-# def login_page(request):
-#      if request.method == 'POST':
-#         username = request.POST.get('username')
-#         password = request.POST.get('password')
-        
-#         user = authenticate(username=username, password=password)
-        
-#         if user:
-#             return redirect(reverse('index'))
-#         else:
-#             print(f"Invalid login details: {username}, {password}")
-#             return HttpResponse("Invalid login details supplied.")
-#      else:
-#             return render(request, 'login.html')
